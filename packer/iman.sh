ba826b2ae87b77cfab88189b77d6e917789064e3
iman <bekoviman9@gmail.com>
2023-03-17 20:14:48 +0000

    packer-test2

--- packer/iman.sh
+++ packer/iman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install software-properties-common gnupg2 curl
-curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg
-sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get install terraform
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install software-properties-common gnupg2 curl -y
+curl https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp.gpg -y
+sudo install -o root -g root -m 644 hashicorp.gpg /etc/apt/trusted.gpg.d/ -y
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
+sudo apt-get install terraform -y
