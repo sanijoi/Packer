8393ae7ffb228fdf55a9b73110931b5e58739523
iman <bekoviman9@gmail.com>
2023-03-22 16:44:55 +0000

    iman2

--- packer/iman.sh
+++ packer/iman.sh
@@ -2,6 +2,10 @@
 
 sleep 30
 
+sudo cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 sudo yum update -y
 sudo yum install httpd -y
 echo "hello world" > iman
